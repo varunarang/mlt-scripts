8c84847d8d3c9bbd6c908095b5655ea84081db8b
Varun Narang <904409+varunarang@users.noreply.github.com>
2023-01-18 21:22:43 +0530

    Update build-melt.sh

--- build/build-melt.sh
+++ build/build-melt.sh
@@ -931,7 +931,7 @@ function get_subproject {
           # No git repo
           debug "No git repo, need to check out"
           feedback_status "Cloning git sources for $1"
-          cmd git --no-pager clone --depth 1 $REPOLOC || die "Unable to git clone source for $1 from $REPOLOC"
+          cmd git --no-pager clone $REPOLOC || die "Unable to git clone source for $1 from $REPOLOC"
           cmd cd $1 || die "Unable to change to directory $1"
           cmd git checkout $REVISION || die "Unable to git checkout $REVISION"
       fi
